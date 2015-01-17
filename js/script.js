d08790f2249aa1006e88f066840692fae6dfe131
yannickoo <drupal@yannickoo.de>
2015-01-18 00:49:23 +0100

    removes github last commit ajax request

--- js/script.js
+++ js/script.js
@@ -1,33 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   var projects = document.querySelectorAll('.projects > div');
 
-  var updateAgo = function(project, json) {
-    var right = project.querySelector('.right'),
-        updated = moment(json.updated_at),
-        date = updated.format('MM/DD/YY'),
-        ago = updated.fromNow(),
-        small = document.createElement('small');
-
-    small.innerHTML = '<a href="https://github.com/' + repo + '" title="' + date + '">' + ago + '</a>';
-    right.appendChild(small);
-  }
-
   for (var i = 0; i < projects.length; i++) {
-    var project = projects[i],
-        repo = project.getAttribute('data-repo'),
-        repoUrl = 'https://api.github.com/repos/' + repo,
-        xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var json = JSON.parse(xhr.responseText);
-
-        updateAgo(projects[i], json);
-      }
-    }
-
-    xhr.open('GET', repoUrl, true);
-    xhr.send();
+    var project = projects[i];
 
     project.style.cursor = 'pointer';
 
