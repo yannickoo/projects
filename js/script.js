16dbd7f0383d04ec8dc741015245e9f89fecba11
yannickoo <github@yannick.md>
2015-04-30 13:47:34 +0200

    adds star button for projects

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var projects = document.querySelectorAll('.projects > div');
 
   for (var i = 0; i < projects.length; i++) {
-    var project = projects[i];
+    var project = projects[i],
+        repo = project.getAttribute('data-repo'),
+        overlayContainer = project.querySelector('.left');
 
     project.style.cursor = 'pointer';
 
@@ -12,5 +14,9 @@ document.addEventListener('DOMContentLoaded', function() {
         location.href = link.getAttribute('href');
       }
     });
+
+    if (repo && overlayContainer) {
+      overlayContainer.innerHTML += '<div class="overlay"><a class="github-button" href="https://github.com/' + repo + '" data-icon="octicon-star" data-style="mega" data-count-href="/' + repo + '/stargazers" data-count-api="/repos/' + repo + '#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star ' + repo + ' on GitHub">Star</a></div>';
+    }
   }
 });
